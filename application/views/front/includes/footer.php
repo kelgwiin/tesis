0d483039f870490e5bac25bd9aaa7c6f8cd8a9f4
unknown <Gustavo@.(none)>
2014-01-22 23:01:33 -0430

    Cambios Para que Se pueda Ver la plantilla de desarrollo!

--- application/views/front/includes/footer.php
+++ application/views/front/includes/footer.php
@@ -1,15 +1,12 @@
 </div><!-- /#wrapper -->
-
     <!-- JavaScript -->
-    <script src="assets/front/sb-admin/js/jquery-1.10.2.js"></script>
-    <script src="assets/front/sb-admin/js/bootstrap.js"></script>
-
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/jquery-1.10.2.js"></script>
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/bootstrap.js"></script>
     <!-- Page Specific Plugins -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
-    <script src="assets/front/sb-admin/js/morris/chart-data-morris.js"></script>
-    <script src="assets/front/sb-admin/js/tablesorter/jquery.tablesorter.js"></script>
-    <script src="assets/front/sb-admin/js/tablesorter/tables.js"></script>
-
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/raphael-min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/morris-0.4.3.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/morris/chart-data-morris.js"></script>
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="<?php echo base_url(); ?>assets/front/sb-admin/js/tablesorter/tables.js"></script>
   </body>
 </html>
