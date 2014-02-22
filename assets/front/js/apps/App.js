1bea4311f0a915427ad2598e3ac89b32806afbba
Kelwin <kelgwiin@gmail.com>
2014-02-21 21:37:36 -0430

    1.-Menu principal miniatura cambiado (modificación de ancho de popover boostrap. 2.- Maquetación de Listado de Componentes de TI al 90% y enlazado alguno de los eventos

--- assets/front/js/apps/App.js
+++ assets/front/js/apps/App.js
@@ -2,9 +2,43 @@ $(document).ready(function()
 {    
     //Crea la instancia del popover
     $('#popoverMenu').popover();
-    //Crea la instancia de los toolitips
+
+    //Crea la instancia de los toolitips en general
     $('[data-toggle=tooltip]').tooltip();
 
+
+    //Botones Componentes de TI
+    //btn Características: Al pulsar el caret que despliega el panel [PANEL]
+    $('a[data-fieldIT=caracteristicas]').on('click', function(){
+        var id  = $(this).attr('data-id');
+
+        //Mostrando el Panel
+        if($('div[data-id='+id+']').attr('class') == 'panel-body hidden') {
+                
+            $('div[data-id='+id+']').attr('class', 'panel-body show');
+            //cambiando el caret
+            $('i#'+id).attr('class','fa fa-caret-down fa-lg');
+        }else{
+
+            $('div[data-id='+id+']').attr('class', 'panel-body hidden');
+            //cambiando el caret
+            $('i#'+id).attr('class','fa fa-caret-right fa-lg');
+        }
+    });
+
+    //btn editar
+    $('a[data-fieldIT=editar]').on('click', function(){
+        var id_editar  = $(this).attr('data-id');
+        alert('editar id - '+id_editar);    
+    });
+
+    //btn eliminar
+    $('a[data-fieldIT=eliminar]').on('click', function(){
+        var id  = $(this).attr('data-id');
+        alert('eliminar id - '+id);    
+    });
+
+    //Fin de Componentes de TI
     	//Mostrando los campos de TI que se despliegan al dar click
     	$("a.fieldsIT").on('click', function(){
         	var rowIndex= $(this).attr("id");//get the row id
@@ -17,16 +51,6 @@ $(document).ready(function()
         	$("i#i"+rowIndex).attr('class',img);
         });
 
-    	//checking the row
-    	$('i[data-ischeck=yes]').on('click',function(){
-    		$('i[data-ischeck=yes]').attr('class','fa fa-square-o');
-    		$(this).attr("class",'fa fa-check-square-o');
-    	});
-
-        //addComponentIT
-        $("button#addComponentIT").on('click',function(){
-        	alert('Hiii, addComponentIT ');
-        });
 
         //editComponentIT
         $("button#editComponentIT").on('click',function(){
@@ -47,4 +71,4 @@ $(document).ready(function()
         });
 
 
- }); 
+    }); 
