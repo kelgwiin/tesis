c03e99abc918f68c66d53f6fc828f1924ee7bf1b
f6rnando <f6rnando@gmail.com>
2014-04-29 12:21:20 -0430

    Setteada la encryption_key en config.php

--- application/config/config.php
+++ application/config/config.php
@@ -225,7 +225,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'XK6DRtn7T7V4Ar6MZ3GF70uPHzflnfs6';
 
 /*
 |--------------------------------------------------------------------------
