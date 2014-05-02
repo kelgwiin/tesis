be98a28f6d635b2ccd0d991a001a53cfd9e2d4ac
unknown <ELIER@.(none)>
2014-05-02 17:34:03 -0430

    cambio de nombre

--- application/modules/Disponibilidad/models/Disponibilidad_model.php
+++ application/modules/Disponibilidad/models/Disponibilidad_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Disponibilidad_model extends CI_Model
+class disponibilidad_model extends CI_Model
 {
 	
 	function __construct()
