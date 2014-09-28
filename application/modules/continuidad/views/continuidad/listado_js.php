6fb1ff568383b4563cc8b44f920690c8109f7b84
Fernando Pinto <f6rnando@gmail.com>
2014-09-28 13:45:34 -0430

    MPDF

---
+++ application/modules/continuidad/views/continuidad/listado_js.php
@@ -0,0 +1,31 @@
+<script>
+	function activate_pcn(id_continuidad,level,state)
+	{
+		$(function()
+		{
+			$.post
+			(
+				'<?php echo site_url('index.php/continuidad/continuidad/activate_pcn') ?>',
+				{id_continuidad:id_continuidad,state:state},
+				function(response)
+				{
+					if(response)
+					{
+						if(state == 2)
+							alert('Plan de Continuidad del Negocio desactivado con éxito');
+						else
+							alert('Plan de Continuidad del Negocio activado con éxito. Las Instrucciones se han enviado a los correos del personal involucrado');
+						
+						var url = "<?php echo site_url('index.php/continuidad/listado_pcn') ?>/"+level;
+						$(location).attr('href',url);
+					}
+					else
+					{
+						alert('Ocurrió un error activando el Plan de Continuidad del Negocio. No se pudo activar.');
+					}
+				}
+			);
+		});
+	}
+	
+</script>
